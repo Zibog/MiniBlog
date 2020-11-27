ddfd7f0efcaaf5ad625eb2b238a97bbe4abf6ec4
Zibog <dmsidak@gmail.com>
2020-11-27 20:30:33 +0300

    Add log out and some assertinons for it

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,16 +1,22 @@
 module SessionsHelper
-    # Осуществляет вход данного пользователя.
+    # Logs in this user
     def log_in(user)
         session[:user_id] = user.id
     end
 
-    # Возвращает текущего вошедшего пользователя (если есть).
+    # Returns current logged user (if exists).
     def current_user
         @current_user ||= User.find_by(id: session[:user_id])
     end
 
-    # Возвращает true, если пользователь вошел, иначе false.
+    # Returns true, if user is logged in, otherwise false.
     def logged_in?
         !current_user.nil?
+    end
+
+    # Logs out the current user
+    def log_out
+        session.delete(:user_id)
+        @current_user = nil
     end
 end
