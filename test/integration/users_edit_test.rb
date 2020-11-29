7105d7624d0152d33002193bad8afbca751e4871
Zibog <dmsidak@gmail.com>
2020-11-29 21:04:25 +0300

    Add a friendly forwarding

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -28,4 +28,19 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name, @user.name
     assert_equal email, @user.email
   end
+
+  test "succsessful edit with friendly forwarding" do
+    get edit_user_path(id: @user.id)
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
+    name = "Misha Ptichkin"
+    email = "mi@sha.pt"
+    patch user_path(@user), params: { user: { name: name, email: email, password: "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
