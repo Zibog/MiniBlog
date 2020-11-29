6c65a9f747b2ddd3a5b243fa229ccbdeeb4020bb
Zibog <dmsidak@gmail.com>
2020-11-29 21:40:21 +0300

    Add a script to generate dummy users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create a main sample user
+User.create!(name: "Example User", email: "dummy@example.com", password: "password",
+                password_confirmation: "password")
+
+# Generate additional users
+49.times do |n|
+    name = Faker::Name.name
+    email = "dummy#{n+1}@example.com"
+    password = "password"
+    User.create!(name: name, email: email, password: password, 
+                    password_confirmation: password)
+end
