3a44d43a75e4eb1f091cdd1350d1690ceb8de272
Zibog <dmsidak@gmail.com>
2020-12-02 00:11:27 +0300

    Another try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,13 +67,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  host = 'secret-headland-76850.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  #host = 'secret-headland-76850.herokuapp.com'
+  #config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
-    :address => 'smtp.sendgrid.com',
+    :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
