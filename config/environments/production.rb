00a562fdbe71487c22326d7b1bc39134b0d44474
Zibog <dmsidak@gmail.com>
2020-12-03 16:39:26 +0300

    Revert account activation because of wrong SendGrid configuration
    This reverts commit db4214e83ae29391aca76f6ab5686318f87156e9.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,8 +67,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  host = 'secret-headland-76850.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  #host = 'secret-headland-76850.herokuapp.com'
+  #config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
     :user_name => ENV['SENDGRID_USERNAME'],
     :password => ENV['SENDGRID_PASSWORD'],
