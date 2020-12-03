e76e0222dce9605a85b709ccc5ef71a0304cc44f
Zibog <dmsidak@gmail.com>
2020-12-03 16:43:15 +0300

    Revert "Add account activation test but config is wrong"
    This reverts commit 08fe05ce136d7a0ecb48a925bf8eb7c82f686abf.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,6 @@ Rails.application.configure do
 
   host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host, protocol: 'http' }
-  #config.action_mailer.default_url_options = { host: "localhost:3000" }
-  #Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 
   config.action_mailer.perform_caching = false
 
