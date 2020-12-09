147b44bd614a000b91039eba5c490872190c02c0
Zibog <dmsidak@gmail.com>
2020-12-09 16:01:07 +0300

    Add post validates and some tests

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:vasya)
+    @post = Post.new(content: "Lorem ipsum dolorem", user_id: @user.id)
+  end
+
+  test "should be valid" do
+    assert @post.valid?
+  end
+
+  test "user id should be present" do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
+
+  test "content should be present" do
+    @post.content = "   "
+    assert_not @post.valid?
+  end
+
+  test "content should be at most 256 chars" do
+    @post.content = "r" * 257
+    assert_not @post.valid?
+  end
 end
