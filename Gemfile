59060f27829c4f3e430147f531c9592dc5359ee4
Zibog <dmsidak@gmail.com>
2020-12-09 22:41:55 +0300

    Add images validation

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'active_storage_validations', '0.8.9'
 gem 'faker',                   '>= 1.4.2'
 gem 'carrierwave',             '>= 0.10.0'
 gem 'mini_magick',             '>= 3.8.0'
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 256 }
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png], message: "Image format must be valid" },
+            size: { less_than: 5.megabytes, message: "File should be less than 5MB" }
 end
