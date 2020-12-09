a39b688d06fbef7ce0ff03355e255cd6398bc42e
Zibog <dmsidak@gmail.com>
2020-12-09 22:13:37 +0300

    Add tests to posts interface

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -5,10 +5,26 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     @post = posts(:lorem)
   end
 
-  test "should redirect if not logged in" do
+  test "should redirect create if not logged in" do
+    assert_no_difference 'Post.count' do
+      post posts_path, params: { post: { content: "Lorem ipsum" } }
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy if not logged in" do
     assert_no_difference 'Post.count' do
       delete post_path(@post)
     end
     assert_redirected_to login_url
+  end
+
+  test "should redirect destroy for wrong micropost" do
+    log_in_as users(:vasya)
+    post = posts(:zone)
+    assert_no_difference 'Post.count' do
+      delete post_path(post)
+    end
+    assert_redirected_to root_url
   end
 end
